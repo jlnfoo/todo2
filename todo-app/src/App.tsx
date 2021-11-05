af8b15a0ef8a4dc199fbdc9503afaaaf6e6ba427
Jaline Foo <jaline.foo@foodpanda.com>
2021-11-05 15:16:23 +0800

    test setPosition to change position of elem in arr

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -7,16 +7,26 @@ import { mockTaskList } from "./mockData";
 // 1. send a mock task to the bottom of the list
 // 2. add a new task to the mock list using an input
 
+// type mockTaskListProps = {
+//   id: number;
+//   taskName: string;
+//   position: number;
+// };
+
 const App = () => {
   const [taskList, setTaskList] = React.useState(mockTaskList);
   const [id, setId] = useState<number>(0);
   const [task, setTask] = useState<string>("");
+  const [position, setPosition] = useState<number>(0);
 
   const handleClick = () => {
-    // (mockTaskList.splice(1));
-    //console.log(mockTaskList);
+    // mockTaskList.push(mockTaskList.shift());
+    // console.log(mockTaskList);
 
-    //mockTaskList.push(mockTaskList.shift());
+    // const clickedTask = mockTaskList.shift();
+    // mockTaskList.push(clickedTask);
+    // console.log(mockTaskList);
+
     //mockTaskList.push(mockTaskList.splice(mockTaskList.indexOf(1), 1)[0]);
 
     /* ???? Argument of type 'number | undefined' is not assignable to parameter of type 'number'.
@@ -29,12 +39,33 @@ const App = () => {
     console.log("works!");
   };
 
+  // const handleClick1 = (mockTaskList) => {
+  //   mockTaskList((task, index) => {
+  //     mockTaskList.splice(index, 1);
+  //     mockTaskList.push
+  //   })
+  // }
+
+  //onclick, set position to be the last in array
+  // const handleClick2 = (e: any) => {
+
+  //   if (mockTaskList.position < mockTaskList.length) {
+  //     mockTaskList.splice(e);
+  //     mockTaskList.push;
+  //     return mockTaskList;
+  //   } else {
+  //     return mockTaskList;
+  //   }
+  // setPosition((mockTaskList) => [...taskList]);
+
+  // };
+
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     setTask(event.target.value);
   };
 
   const addTask = (): void => {
-    const newTask = { id: id, taskName: task };
+    const newTask = { id: id, taskName: task, position: position };
     setTaskList([...taskList, newTask]);
     setTask("");
   };
--- todo-app/src/mockData.ts
+++ todo-app/src/mockData.ts
@@ -1,6 +1,6 @@
 export const mockTaskList = [
-    { id: 1, taskName: "hello" },
-    { id: 2, taskName: "goodbye" },
-    { id: 3, taskName: "hello & goodbye" },
-    { id: 4, taskName: "foo" }
+    { id: 1, taskName: "hello", position: 11 },
+    { id: 2, taskName: "goodbye", position: 22 },
+    { id: 3, taskName: "hello & goodbye", position: 33 },
+    { id: 4, taskName: "foo", position:44 }
   ];
