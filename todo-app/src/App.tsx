be91c49796cb4e3f3642d93833a030945d602f03
Jaline Foo <jaline.foo@foodpanda.com>
2021-11-11 16:32:57 +0800

    updated add to completed list func

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -14,7 +14,7 @@ type MyTask = {
 const App = () => {
   const [todoList, setTodoList] = React.useState<MyTask[]>(mockTaskList);
   const [task, setTask] = useState("");
-  const [completedList, updateCompletedList] = useState([]);
+  const [completedList, updateCompletedList] = React.useState<MyTask[]>([]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     setTask(event.target.value);
@@ -30,24 +30,39 @@ const App = () => {
   //onclick event for adding completed task to completed list
   const addToCompletedList = (taskId: number | undefined) => {
     const completedList = [...todoList];
-    completedList.push(taskId);
-    return completedList;
-  };
 
-  //onclick event for removing completed task to completed list, and pushing it back to todolist
-  const removeFromCompletedList = (taskId: number | undefined) => {
-    const completedList = [...todoList]; // rest of Task List
-    const taskIndex = completedList.indexOf(taskId); // find index of task
-
-    if (taskIndex >= 0) {
-      //if taskIndex of what we want to remove exists,
-      const removedTask = completedList.splice(taskIndex, 1); //remove task from list,
-      console.log(removedTask);
-      todoList.push(removedTask); //add it back to todo list
-      updateCompletedList(completedList); // update completed task list
-      setTodoList([...todoList, removedTask]); //update todo list
+    for (let i = 0; i < completedList.length; i++) {
+      const allIndex = completedList.map((task) => task.id); //returns array of id - numbers
+      //id(number), taskId (number)
+      if (allIndex[i] === taskId) {
+        //filters object with same id as target, returns as array of object
+        const taskMatched = completedList.filter((task) => {
+          return task.id === taskId;
+        });
+        console.log(taskMatched);
+        completedList.concat(taskMatched);
+      }
     }
+    updateCompletedList([...completedList]); //IS THE ERROR HERE?
   };
+
+  // //onclick event for removing completed task to completed list, and pushing it back to todolist
+  // const removeFromCompletedList = (taskId: number | undefined) => {
+  //   const completedList = [...todoList]; // rest of Task List
+
+  //   //  Argument of type 'number | undefined' is not assignable to parameter of type 'MyTask'.
+  //   //  Type 'undefined' is not assignable to type 'MyTask'.ts(2345)
+  //   const taskIndex = completedList.indexOf(taskId); // find index of task
+
+  //   if (taskIndex >= 0) {
+  //     //if taskIndex of what we want to remove exists,
+  //     const removedTask = completedList.splice(taskIndex, 1); //remove task from list,
+  //     console.log(removedTask);
+  //     todoList.push(removedTask); //add it back to todo list
+  //     updateCompletedList(completedList); // update completed task list
+  //     setTodoList([...todoList, removedTask]); //update todo list
+  //   }
+  // };
 
   return (
     <div>
@@ -70,9 +85,8 @@ const App = () => {
       <h1> Completed List</h1>
       <ul>
         {completedList.map((task) => {
-          <li onClick={() => removeFromCompletedList(task.id)}>
-            {task.taskName}
-          </li>;
+          // <li onClick={() => removeFromCompletedList(task.id)}>
+          <li>{task.taskName}</li>;
         })}
       </ul>
     </div>
