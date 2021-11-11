21b470be0946efea9a4ad2000ebc96135a51abb3
Jaline Foo <jaline.foo@foodpanda.com>
2021-11-11 16:36:57 +0800

    created add to completed list function

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -46,24 +46,6 @@ const App = () => {
     updateCompletedList([...completedList]);
   };
 
-  // //onclick event for removing completed task to completed list, and pushing it back to todolist
-  // const removeFromCompletedList = (taskId: number | undefined) => {
-  //   const completedList = [...todoList]; // rest of Task List
-
-  //   //  Argument of type 'number | undefined' is not assignable to parameter of type 'MyTask'.
-  //   //  Type 'undefined' is not assignable to type 'MyTask'.ts(2345)
-  //   const taskIndex = completedList.indexOf(taskId); // find index of task
-
-  //   if (taskIndex >= 0) {
-  //     //if taskIndex of what we want to remove exists,
-  //     const removedTask = completedList.splice(taskIndex, 1); //remove task from list,
-  //     console.log(removedTask);
-  //     todoList.push(removedTask); //add it back to todo list
-  //     updateCompletedList(completedList); // update completed task list
-  //     setTodoList([...todoList, removedTask]); //update todo list
-  //   }
-  // };
-
   return (
     <div>
       <h1>Add Tasks</h1>
@@ -85,7 +67,6 @@ const App = () => {
       <h1> Completed List</h1>
       <ul>
         {completedList.map((task) => {
-          // <li onClick={() => removeFromCompletedList(task.id)}>
           <li>{task.taskName}</li>;
         })}
       </ul>
