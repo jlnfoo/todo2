2f1dd77d4c31255de5eff8138ac58024983dfa4a
Jaline Foo <jaline.foo@foodpanda.com>
2021-11-12 15:25:17 +0800

    added shift up function

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -24,6 +24,7 @@ type MyTask = {
 const App = () => {
   const [taskList, setTaskList] = React.useState<MyTask[]>(mockTaskList);
   const [task, setTask] = useState("");
+  const newMainList: MyTask[] = [];
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     setTask(event.target.value);
@@ -37,24 +38,45 @@ const App = () => {
 
   const shiftDown = (taskId: number | undefined) => {
     if (taskId === undefined) return;
-
-    const newMainList: MyTask[] = [];
     let removedItem;
 
+    //loop through objects in taskList
     for (let i = 0; i < taskList.length; i++) {
+      //set new variable name for each object
       const myCurrentTask = taskList[i];
 
+      //if id of object matches taskId
       if (myCurrentTask.id === taskId) {
+        //give that object a new var name
         removedItem = myCurrentTask;
       } else {
+        //if id of object doesnt match taskId, push that object to newMainList
         newMainList.push(myCurrentTask);
       }
     }
-
+    // if object matches taskId, we give it a new var name of removedItem, then push this removedItem to end of newMainList
     if (removedItem) {
       newMainList.push(removedItem);
     }
 
+    setTaskList(newMainList);
+  };
+
+  const shiftUp = (taskId: number | undefined) => {
+    if (taskId === undefined) return;
+    let removed;
+
+    for (let j = 0; j < taskList.length; j++) {
+      if (taskList[j].id === taskId) {
+        removed = taskList[j];
+      } else {
+        newMainList.push(taskList[j]);
+      }
+    }
+
+    if (removed) {
+      newMainList.unshift(removed);
+    }
     setTaskList(newMainList);
   };
 
@@ -74,7 +96,7 @@ const App = () => {
         {taskList.map((task) => (
           <li>
             {task.taskName}
-            <button>Up</button>
+            <button onClick={() => shiftUp(task.id)}>Up</button>
             <button onClick={() => shiftDown(task.id)}>Down</button>
           </li>
         ))}
