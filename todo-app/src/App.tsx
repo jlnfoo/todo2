75e88789e539bca2f6db0af6fb762bdee29cf28d
Jaline Foo <jaline.foo@foodpanda.com>
2021-11-12 15:26:10 +0800

    removed comments

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -3,18 +3,6 @@ import "./App.css";
 
 import { mockTaskList } from "./mockData";
 
-// TODOs
-// 1. send a mock task to the bottom of the list
-
-/**
- * export const mockTaskList = [
-    { id: 1, taskName: "hello", complete: false },
-    { id: 2, taskName: "goodbye", complete: false},
-    { id: 3, taskName: "hello & goodbye", complete: false },
-    { id: 4, taskName: "foo" , complete: false}
-  ];
- */
-
 type MyTask = {
   id?: number; // number | undefined
   taskName: string;
